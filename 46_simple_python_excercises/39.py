014d2433aec3c0f825d45e6e250247c3d3a0e4dc
Exorades <exorades@gmail.com>
2014-03-14 10:01:17 +0100

    Corrected guess input. Removed unnecesary gues_taken statment. Re designed print.

--- 46_simple_python_excercises/39.py
+++ 46_simple_python_excercises/39.py
@@ -4,21 +4,19 @@ guesses_taken = 0
 print('Hello! What is your name?')
 name = raw_input()
 number = random.randint(1, 20)
-guess = ''
-print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
+guess = 0
+print('Well, {0}, I am thinking of a number between 1 and 20.').format(name)
 
 
 print('Take a guess.')
 
 while guess != number:
-    guess = input()
-    guess = int(guess)
+    guess = int(input())
+    guesses_taken += 1
     if guess > number:
-        guesses_taken = guesses_taken + 1
         print('Your guess is too high.')
     elif guess < number:
-        guesses_taken = guesses_taken + 1
         print('Your guess is too low.')
     else:
-        guesses_taken = guesses_taken + 1
-        print('Good job, %s! You guessed my number in %d guesses!') % (name, guesses_taken)
+        print('Good job, {0}!').format(name)
+        print('You guessed my number in {0} guesses!').format(guesses_taken)
